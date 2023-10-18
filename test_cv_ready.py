b7cf611391ad184b94d67af48781c34ad86c4780
mwright-pivotal <wrightmi@vmware.com>
2023-10-18 19:43:09 -0400

    update tests

--- test_cv_ready.py
+++ test_cv_ready.py
@@ -2,6 +2,10 @@ from openvino.runtime import Core, Model
 from PIL import Image
 from numpy import asarray
 import numpy as np
+from typing import Tuple, Dict
+import cv2, time
+import notebook_utils as utils
+import blackjack_backend as blackjack
 
 def load_model(modelpath):
     core = Core()
@@ -26,18 +30,93 @@ def image_to_tensor(image:np.ndarray):
         input_tensor = np.expand_dims(input_tensor, 0)
     return input_tensor
 
+def letterbox(img: np.ndarray, new_shape:Tuple[int, int] = (640, 640), color:Tuple[int, int, int] = (114, 114, 114), auto:bool = False, scale_fill:bool = False, scaleup:bool = False, stride:int = 32):
+    """
+    Resize image and padding for detection. Takes image as input, 
+    resizes image to fit into new shape with saving original aspect ratio and pads it to meet stride-multiple constraints
+    
+    Parameters:
+      img (np.ndarray): image for preprocessing
+      new_shape (Tuple(int, int)): image size after preprocessing in format [height, width]
+      color (Tuple(int, int, int)): color for filling padded area
+      auto (bool): use dynamic input size, only padding for stride constrins applied
+      scale_fill (bool): scale image to fill new_shape
+      scaleup (bool): allow scale image if it is lower then desired input size, can affect model accuracy
+      stride (int): input padding stride
+    Returns:
+      img (np.ndarray): image after preprocessing
+      ratio (Tuple(float, float)): hight and width scaling ratio
+      padding_size (Tuple(int, int)): height and width padding size
+    
+    
+    """
+    # Resize and pad image while meeting stride-multiple constraints
+    shape = img.shape[:2]  # current shape [height, width]
+    if isinstance(new_shape, int):
+        new_shape = (new_shape, new_shape)
+
+    # Scale ratio (new / old)
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    if not scaleup:  # only scale down, do not scale up (for better test mAP)
+        r = min(r, 1.0)
+
+    # Compute padding
+    ratio = r, r  # width, height ratios
+    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+    if auto:  # minimum rectangle
+        dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
+    elif scale_fill:  # stretch
+        dw, dh = 0.0, 0.0
+        new_unpad = (new_shape[1], new_shape[0])
+        ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
+
+    dw /= 2  # divide padding into 2 sides
+    dh /= 2
+
+    if shape[::-1] != new_unpad:  # resize
+        img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
+    return img, ratio, (dw, dh)
+
+
+def preprocess_image(img0: np.ndarray):
+    """
+    Preprocess image according to YOLOv8 input requirements. 
+    Takes image in np.array format, resizes it to specific size using letterbox resize and changes data layout from HWC to CHW.
+    
+    Parameters:
+      img0 (np.ndarray): image for preprocessing
+    Returns:
+      img (np.ndarray): image after preprocessing
+    """
+    # resize
+    img = letterbox(img0)[0]
+    
+    # Convert HWC to CHW
+    img = img.transpose(2, 0, 1)
+    img = np.ascontiguousarray(img)
+    return img
+
 def test_answer():
     from PIL import Image
     from numpy import asarray
     # load the image
     image = Image.open('card.jpg')
-    data = asarray(image)
-    print(type(data))
-
+    # Get the results.
+    input_image = np.array(image)
+    
     model = load_model("card-detect-0728_openvino_model/card-detect-0728.xml")
+    start_time = time.time()
+    # model expects RGB image, while video capturing in BGR
+    detections = blackjack.detect(input_image[:, :, ::-1], model)[0]
+    stop_time = time.time()
+    boxes = detections["det"]
+    h, w = input_image.shape[:2]
+    for idx, (*xyxy, conf, lbl) in enumerate(boxes):
+        print(int(lbl))
 
-    image_tensor = image_to_tensor(data)
-    result = model(image_tensor)
-    boxes = result[model.output(0)]
-    print(boxes)
-    assert model
+    #15 is 4 of Spades
+    assert int(lbl)==15
